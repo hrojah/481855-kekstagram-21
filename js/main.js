278d9f38d5da60818f2f63f41fbf0a03eac617c1
Vladimir Plotnikov <hrojah@gmail.com>
2020-09-29 00:10:11 +0500

    Добавляет правки

--- js/main.js
+++ js/main.js
@@ -6,19 +6,19 @@ const MIN_AVATAR = 1;
 const MAX_AVATAR = 6;
 
 const COMMENT_TEXT = [
-  'Всё отлично!',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  `Всё отлично!`,
+  `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
+  `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
+  `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`
 ];
 
 const USER_NAMES = [
-  'Антон',
-  'Олег',
-  'Владимир',
-  'Игорь',
-  'Аркадий',
-  'Дмитрий',
+  `Антон`,
+  `Олег`,
+  `Владимир`,
+  `Игорь`,
+  `Аркадий`,
+  `Дмитрий`
 ];
 
 const getRandomNumber = (min, max) => {
@@ -26,7 +26,7 @@ const getRandomNumber = (min, max) => {
 };
 
 const getCommentText = () => {
-  return  COMMENT_TEXT[getRandomNumber(0, COMMENT_TEXT.length)];
+  return COMMENT_TEXT[getRandomNumber(0, COMMENT_TEXT.length)];
 };
 
 const getUserName = () => {
@@ -35,13 +35,13 @@ const getUserName = () => {
 
 const getComment = () => {
   const comment = [];
-  const commentNumber = getRandomNumber(1,5);
+  const commentNumber = getRandomNumber(1, 5);
   for (let i = 1; i <= commentNumber; i++) {
     comment.push({
       avatar: `img/avatar-${getRandomNumber(MIN_AVATAR, MAX_AVATAR)}.svg`,
       message: getCommentText(),
       name: getUserName()
-      });
+    });
   }
   return comment;
 };
@@ -49,13 +49,12 @@ const getComment = () => {
 const createPhotoDescriptionArray = (values) => {
   const photoDescription = [];
   for (let i = 1; i <= values; i++) {
-    photoDescription.push(
-      {
-        url: `photos/${i}.jpg`,
-        description: '',
-        likes : getRandomNumber(MIN_LIKES, MAX_LIKES),
-        comments: getComment()
-      });
+    photoDescription.push({
+      url: `photos/${i}.jpg`,
+      description: ``,
+      likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
+      comments: getComment()
+    });
   }
   return photoDescription;
 };
@@ -63,15 +62,15 @@ const createPhotoDescriptionArray = (values) => {
 const renderPicture = (photoDescription) => {
   const pictureElement = cardsPicture.cloneNode(true);
 
-  pictureElement.querySelector('.picture__img').src = photoDescription.url;
-  pictureElement.querySelector('.picture__likes').textContent = photoDescription.likes;
-  pictureElement.querySelector('.picture__comments').textContent = photoDescription.comments.length;
+  pictureElement.querySelector(`.picture__img`).src = photoDescription.url;
+  pictureElement.querySelector(`.picture__likes`).textContent = photoDescription.likes;
+  pictureElement.querySelector(`.picture__comments`).textContent = photoDescription.comments.length;
 
   return pictureElement;
 };
 
-const cardsPicture = document.querySelector('#picture').content.querySelector('a');
-const pictures = document.querySelector('.pictures');
+const cardsPicture = document.querySelector(`#picture`).content.querySelector(`a`);
+const pictures = document.querySelector(`.pictures`);
 const pictureFragment = document.createDocumentFragment();
 
 const photoDescription = createPhotoDescriptionArray(25);
