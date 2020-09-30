2a2d1ff53322ffe309b466cef3e1907fcacbdcae
Vladimir Plotnikov <hrojah@gmail.com>
2020-09-30 21:07:49 +0500

    Добавляет правки

--- js/main.js
+++ js/main.js
@@ -33,9 +33,8 @@ const getUserName = () => {
   return USER_NAMES[getRandomNumber(0, USER_NAMES.length)];
 };
 
-const getComment = () => {
+const getComments = (commentNumber) => {
   const comment = [];
-  const commentNumber = getRandomNumber(1, 5);
   for (let i = 1; i <= commentNumber; i++) {
     comment.push({
       avatar: `img/avatar-${getRandomNumber(MIN_AVATAR, MAX_AVATAR)}.svg`,
@@ -53,11 +52,12 @@ const createPhotoDescriptionArray = (values) => {
       url: `photos/${i}.jpg`,
       description: ``,
       likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
-      comments: getComment()
+      comments: getComments(getRandomNumber(1, 5))
     });
   }
   return photoDescription;
 };
+
 
 const renderPicture = (photoDescription) => {
   const pictureElement = cardsPicture.cloneNode(true);
@@ -67,6 +67,21 @@ const renderPicture = (photoDescription) => {
   pictureElement.querySelector(`.picture__comments`).textContent = photoDescription.comments.length;
 
   return pictureElement;
+};
+
+const renderComment = () => {
+  const element = document.createElement(`li`);
+  const commentImg = document.createElement(`img`);
+  const commentText = document.createElement(`p`);
+  element.classList.add(`social.comment`);
+  commentImg.classList.add(`social__picture`);
+  commentText.classList.add(`social.text`);
+  element.append(`commentImg`);
+  element.append(`commentText`);
+  commentImg.src = photoDescription.comments.avatar;
+  commentImg.alt = photoDescription.comments.name;
+  commentText.textContent = photoDescription.comments.message;
+  return element;
 };
 
 const cardsPicture = document.querySelector(`#picture`).content.querySelector(`a`);
@@ -84,18 +99,21 @@ pictures.append(pictureFragment);
 const bigPicture = document.querySelector(`.big-picture`);
 const commentCount = bigPicture.querySelector(`.social__comment-count`);
 const commentLoader = bigPicture.querySelector(`.comments-loader`);
-const socialPicture = bigPicture.querySelector(`.social__picture`);
+/*const socialComments = document.querySelector(`.social__comments`);*/
 
-bigPicture.classList.remove(`hidden`);
+const renderBigPicture = () => {
+  bigPicture.querySelector(`.big-picture__img img`).src = photoDescription.url;
+  bigPicture.querySelector(`.likes-count`).textContent = photoDescription.likes;
+  bigPicture.querySelector(`.social__caption`).textContent = photoDescription.description;
+  bigPicture.querySelector(`.comments-count`).textContent = photoDescription.comments.length;
+  for (let i = 0; i <= photoDescription.comments.length; i++) {
+    renderComment(photoDescription.comments[i]);
+  }
+};
 
-bigPicture.querySelector(`div > img`).src.textContent = photoDescription[0].url;
-bigPicture.querySelector(`.likes-count`).textContent = photoDescription[0].likes;
-bigPicture.querySelector(`.social__caption`).textContent = photoDescription[0].description;
-bigPicture.querySelector(`.comments-count`).textContent = photoDescription[0].comments.length;
-socialPicture.src = photoDescription[0].comments.avatar;
-socialPicture.alt.textContent = photoDescription[0].comments.name;
-bigPicture.querySelector(`.social__text`).textContent = photoDescription[0].comments.message;
-
+renderBigPicture(photoDescription[0]);
 
 commentCount.classList.add(`hidden`);
 commentLoader.classList.add(`hidden`);
+
+bigPicture.classList.remove(`hidden`);
